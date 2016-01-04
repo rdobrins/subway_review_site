9ae8d35be1c3aa5ea3de7f2417959eb3f2b36914
Tom Fafard <tfafard@Toms-MacBook-Pro.local>
2016-01-04 16:18:58 -0500

    added schema

---
+++ db/schema.rb
@@ -0,0 +1,41 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20160104200908) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "reviews", force: :cascade do |t|
+    t.string  "rating",     null: false
+    t.text    "body"
+    t.integer "up_votes",   null: false
+    t.integer "down_votes", null: false
+    t.integer "user_id",    null: false
+    t.integer "station_id", null: false
+  end
+
+  create_table "stations", force: :cascade do |t|
+    t.string "name",    null: false
+    t.string "address", null: false
+    t.string "city",    null: false
+    t.string "state",   null: false
+    t.string "zip",     null: false
+  end
+
+  create_table "user_reviews", force: :cascade do |t|
+    t.integer "review_id", null: false
+    t.integer "user_id",   null: false
+  end
+
+end
