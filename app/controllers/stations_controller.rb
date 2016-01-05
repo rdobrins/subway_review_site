828b54cc31d8134657241d05b27ff8710db08d3c
Ryan Dobrinski <ryandobrinski@Macintosh-2.local>
2016-01-05 17:02:39 -0500

    added tests to stations display and adding station

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -4,11 +4,26 @@ class StationsController < ApplicationController
   end
 
   def show
+    @station = Station.find(params[:id])
   end
 
   def new
+    @station = Station.new
   end
 
   def create
+    @station = Station.new(station_params)
+
+    if @station.save
+      redirect_to station_path(@station)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def station_params
+    params.require(:station).permit(:name, :address, :city, :state, :zip)
   end
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -1,3 +1,11 @@
 class Station < ActiveRecord::Base
   has_many :reviews
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip, presence: true
+  validates :zip, length: { is: 5}
+  validates :zip, numericality: { only_integer: true }
 end
