7adcc80a51374d355e4a4b5284204c47cba969cc
vermartian <vermartian@gmail.com>
2016-01-05 11:37:04 -0500

    4th Hound F.A.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,5 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
-  validates :column, format: value :email,
-  with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create
+  validates :email, format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 end
