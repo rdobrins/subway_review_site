4b1933252692e9b5b6f5a2d8dff50d7deb79aa2a
vermartian <vermartian@gmail.com>
2016-01-05 11:40:52 -0500

    5th

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable,
-         :registerable,
-         :recoverable,
-         :rememberable,
-         :trackable,
-         :validatable
+    :registerable,
+    :recoverable,
+    :rememberable,
+    :trackable,
+    :validatable
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
