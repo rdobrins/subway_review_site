9270f0c2fa388319cd251939cdc3d2646485465f
vermartian <vermartian@gmail.com>
2016-01-05 11:31:47 -0500

    Third Hound Fix Attempt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+  validates :column, format: value :email,
+  with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,6 @@
 Devise.setup do |config|
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  temp = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = temp
   require 'devise/orm/active_record'
   config.case_insensitive_keys = [:email]
   config.strip_whitespace_keys = [:email]
