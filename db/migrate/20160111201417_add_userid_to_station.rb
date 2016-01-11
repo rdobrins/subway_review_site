b2bcb0ffaa8859a6f5a63327ebe619716053afcf
Sebastian <smkopp92@gmail.com>
2016-01-11 17:14:03 -0500

    hound 4

--- db/migrate/20160111201417_add_userid_to_station.rb
+++ db/migrate/20160111201417_add_userid_to_station.rb
@@ -2,7 +2,7 @@ class AddUseridToStation < ActiveRecord::Migration
   def up
     add_column :stations, :user_id, :integer, null: false
   end
-  
+
   def down
     remove_column :stations, :user_id
   end
