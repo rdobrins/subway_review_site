67b3abe9162738fc79203a27fdab715858986af1
vermartian <vermartian@gmail.com>
2016-01-11 13:04:00 -0500

    hound resolutions

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -2,12 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Station, type: :model do
   let(:station) { FactoryGirl.create(:station) }
-  let(:review1) {
-    FactoryGirl.create(:review, rating: 5, station_id: station.id) }
-  let(:review2) {
-    FactoryGirl.create(:review, rating: 4, station_id: station.id) }
-  let(:review3) {
-    FactoryGirl.create(:review, rating: 2, station_id: station.id) }
+  let(:review1) do
+    FactoryGirl.create(:review, rating: 5, station_id: station.id)
+  end
+  let(:review2) do
+    FactoryGirl.create(:review, rating: 4, station_id: station.id)
+  end
+  let(:review3) do
+    FactoryGirl.create(:review, rating: 2, station_id: station.id)
+  end
   it 'has an average_rating method' do
     station
     review1
