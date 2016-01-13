72d298d4d4cdfd7eb4e13d0b615022b6f2ccf019
Sebastian <smkopp92@gmail.com>
2016-01-13 16:04:52 -0500

    I LOVE HOUND

--- app/assets/javascripts/vote.js
+++ app/assets/javascripts/vote.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
   $(".vote").on("ajax:complete", function(a, b){
     var response = JSON.parse(b.responseText);
     var parentId = a.currentTarget.parentNode.id;
-    var c = ""
     a = $("#" + parentId + " > .votes");
     a.empty();
-    a.append("Upvotes: " + response.review["up_votes"] + " Downvotes: " + response.review["down_votes"]);
-    if(response.temp == null){
+    a.append("Upvotes: " + response.review.up_votes);
+    a.append(" Downvotes: " + response.review.down_votes);
+    if(response.temp === null){
       if(response.vote.up === true){
         if($("#" + parentId + " > #upvote input").length > 1){
           var c = $("#" + parentId + " > #upvote input");
@@ -27,12 +27,12 @@ $(document).ready(function(){
     }else{
       if(response.temp.up === true){
         if($("#" + parentId + " > #upvote input").length > 1){
-          var c = $("#" + parentId + " > #upvote input");
+          var d = $("#" + parentId + " > #upvote input");
         }else{
-          var c = $("#" + parentId + " > #upvoted input");
+          var d = $("#" + parentId + " > #upvoted input");
         };
-        c.css("background-color","white");
-        c.css("color","green");
+        d.css("background-color","white");
+        d.css("color","green");
         if(response.vote.up != true){
           if($("#" + parentId + " > #downvote input").length > 1){
             var c = $("#" + parentId + " > #downvote input");
@@ -44,12 +44,12 @@ $(document).ready(function(){
         }
       }else{
         if($("#" + parentId + " > #downvote input").length > 1){
-          var c = $("#" + parentId + " > #downvote input");
+          var d = $("#" + parentId + " > #downvote input");
         }else{
-          var c = $("#" + parentId + " > #downvoted input");
+          var d = $("#" + parentId + " > #downvoted input");
         };
-        c.css("background-color","white");
-        c.css("color","red");
+        d.css("background-color","white");
+        d.css("color","red");
         if(response.vote.up === true){
           if($("#" + parentId + " > #upvote input").length > 1){
             var c = $("#" + parentId + " > #upvote input");
@@ -59,7 +59,7 @@ $(document).ready(function(){
           c.css("background-color","green");
           c.css("color","white");
         };
-      };
-    };
+      }
+    }
   });
 });
