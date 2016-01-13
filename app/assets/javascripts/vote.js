7a4174c1facfba4377500e4915ee76c5809dafbf
Sebastian <smkopp92@gmail.com>
2016-01-13 16:07:52 -0500

    Please Hound

--- app/assets/javascripts/vote.js
+++ app/assets/javascripts/vote.js
@@ -2,24 +2,25 @@ $(document).ready(function(){
   $(".vote").on("ajax:complete", function(a, b){
     var response = JSON.parse(b.responseText);
     var parentId = a.currentTarget.parentNode.id;
+    var c = ""
     a = $("#" + parentId + " > .votes");
     a.empty();
-    a.append("Upvotes: " + response.review.up_votes);
-    a.append(" Downvotes: " + response.review.down_votes);
-    if(response.temp === null){
+    a.append("Upvotes: " + response.review.up_votes + " Downvotes: ");
+    a.append(response.review.down_votes);
+    if(response.temp == null){
       if(response.vote.up === true){
         if($("#" + parentId + " > #upvote input").length > 1){
-          var c = $("#" + parentId + " > #upvote input");
+          c = $("#" + parentId + " > #upvote input");
         }else{
-          var c = $("#" + parentId + " > #upvoted input");
+          c = $("#" + parentId + " > #upvoted input");
         };
         c.css("background-color","green");
         c.css("color","white");
       }else{
         if($("#" + parentId + " > #downvote input").length > 1){
-          var c = $("#" + parentId + " > #downvote input");
+          c = $("#" + parentId + " > #downvote input");
         }else{
-          var c = $("#" + parentId + " > #downvoted input");
+          c = $("#" + parentId + " > #downvoted input");
         };
         c.css("background-color","red");
         c.css("color","white");
@@ -27,39 +28,39 @@ $(document).ready(function(){
     }else{
       if(response.temp.up === true){
         if($("#" + parentId + " > #upvote input").length > 1){
-          var d = $("#" + parentId + " > #upvote input");
+          c = $("#" + parentId + " > #upvote input");
         }else{
-          var d = $("#" + parentId + " > #upvoted input");
+          c = $("#" + parentId + " > #upvoted input");
         };
-        d.css("background-color","white");
-        d.css("color","green");
+        c.css("background-color","white");
+        c.css("color","green");
         if(response.vote.up != true){
           if($("#" + parentId + " > #downvote input").length > 1){
-            var c = $("#" + parentId + " > #downvote input");
+            c = $("#" + parentId + " > #downvote input");
           }else{
-            var c = $("#" + parentId + " > #downvoted input");
+            c = $("#" + parentId + " > #downvoted input");
           };
           c.css("background-color","red");
           c.css("color","white");
         }
       }else{
         if($("#" + parentId + " > #downvote input").length > 1){
-          var d = $("#" + parentId + " > #downvote input");
+          c = $("#" + parentId + " > #downvote input");
         }else{
-          var d = $("#" + parentId + " > #downvoted input");
+          c = $("#" + parentId + " > #downvoted input");
         };
-        d.css("background-color","white");
-        d.css("color","red");
+        c.css("background-color","white");
+        c.css("color","red");
         if(response.vote.up === true){
           if($("#" + parentId + " > #upvote input").length > 1){
-            var c = $("#" + parentId + " > #upvote input");
+            c = $("#" + parentId + " > #upvote input");
           }else{
-            var c = $("#" + parentId + " > #upvoted input");
+            c = $("#" + parentId + " > #upvoted input");
           };
           c.css("background-color","green");
           c.css("color","white");
         };
-      }
-    }
+      };
+    };
   });
 });
