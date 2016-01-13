8533184e4fd52b39a86504fabecd2e10353f60e4
Ryan Dobrinski <ryandobrinski@Macintosh-2.local>
2016-01-13 14:37:24 -0500

    fixing hound problems

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -13,8 +13,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
   def default_url
-      "http://i.imgur.com/zelfHo9.png"
-   end
+    "http://i.imgur.com/zelfHo9.png"
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
