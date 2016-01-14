ac7c6e2023cebf66abf2846e80e4d15cf2fff8fb
Ryan Dobrinski <ryandobrinski@Macintosh-2.local>
2016-01-14 15:36:32 -0500

    fixing rake issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,41 +1,41 @@
-# User.create(
-#   first_name: "Bob",
-#   last_name: "Saget",
-#   email: "b@gmail.com",
-#   password: "password"
-# )
-#
-# 30.times do |i|
-#   Station.create(
-#     name: "Chinatown Station #{i}",
-#     address: "33 Harrison Ave",
-#     city: "Boston",
-#     state: "MA",
-#     zip: "02134",
-#     user_id: 1
-#   )
-# end
-#
-# Station.create(
-#   name: "Arlington Station",
-#   address: "399 Boylston St",
-#   city: "Boston",
-#   state: "MA",
-#   zip: "01234",
-#   user_id: 1
-# )
-#
-# User.create(
-#   first_name: "Test",
-#   last_name: "Admin",
-#   email: "admin@gmail.com",
-#   password: "password",
-#   role: "admin"
-# )
-#
-# Review.create(
-#   body: "review for station with id one and a user with id one",
-#   user_id: 1,
-#   station_id: 1,
-#   rating: 4
-# )
+User.create(
+  first_name: "Bob",
+  last_name: "Saget",
+  email: "b@gmail.com",
+  password: "password"
+)
+
+30.times do |i|
+  Station.create(
+    name: "Chinatown Station #{i}",
+    address: "33 Harrison Ave",
+    city: "Boston",
+    state: "MA",
+    zip: "02134",
+    user_id: 1
+  )
+end
+
+Station.create(
+  name: "Arlington Station",
+  address: "399 Boylston St",
+  city: "Boston",
+  state: "MA",
+  zip: "01234",
+  user_id: 1
+)
+
+User.create(
+  first_name: "Test",
+  last_name: "Admin",
+  email: "admin@gmail.com",
+  password: "password",
+  role: "admin"
+)
+
+Review.create(
+  body: "review for station with id one and a user with id one",
+  user_id: 1,
+  station_id: 1,
+  rating: 4
+)
